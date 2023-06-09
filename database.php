770257be371deb35009b46be6713a3db99fb0ace
YanSeg <yan.seguin@le-campus-numerique.fr>
2023-06-09 14:11:25 +0200

    creation des fonctions

--- database.php
+++ database.php
@@ -7,32 +7,53 @@ try {
         die('Erreur : ' . $e->getMessage());
 }
 
-$productsStatement = $db->prepare('SELECT * FROM products');
-$productsStatement->execute();
-$products = $productsStatement->fetchAll();
 
 
-$sqlQuery= "SELECT * FROM `products` WHERE `availability`!=1";
-$productQuantityNull = $db->prepare($sqlQuery);
-$productQuantityNull->execute();
-$productsQuantity = $productQuantityNull->fetchAll();
+// _____________________________________________________________________________________ Les fonctions "méthodes"
+function QueryTable($db, $sqlQuery)
+{
+        $fonct = $db->prepare($sqlQuery);
+        $fonct->execute();
+        $fonct = $fonct->fetchAll();
+        return $fonct;
+}
 
-//fetch veut dire <va chercher>
+
+function AffichageTables($fonct) {
+        foreach ($fonct as $fonct)
+       var_dump ($fonct);
+
+}
 
 
-$sqlQuery5= 
-"SELECT  products.productname, products.productprice, order_product.quantity, orders.id 
+// _____________________________________________________________________________________ Liste des requêtes 
+
+
+$sqlQuery1 = "SELECT * FROM products";
+$sqlQuery2 = "SELECT * FROM `products` WHERE `availability`=0";
+
+$sqlQuery5 =  "SELECT  products.productname, products.productprice, order_product.quantity, orders.id 
 FROM orders
 INNER JOIN order_product ON order_product.orders_id = orders.id
 INNER JOIN products ON order_product.products_id = products.id
 WHERE orders.id = 1";
 
-$productid1 = $db->prepare($sqlQuery5);
-$productid1->execute();
-$productid1 = $productid1->fetchAll();
+
+$sqlQuery6 = "SELECT orders.ordernumber, products.productprice * order_product.quantity AS 'Prix total de la commande'
+FROM orders
+JOIN order_product ON order_product.orders_id = orders.id
+JOIN products ON order_product.products_id = products.id
+";
+
+
+$sqlQuery7 = "INSERT INTO orders (ordernumber, orderdate, total, customers_id) VALUES (7, 2023-06-08,0, 2) ";
+$sqlQuery8 = "DELETE FROM orders WHERE ordernumber=7";
+
+// _____________________________________________________________________________________Liste des requêtes êtes 
 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -43,14 +64,13 @@ $productid1 = $productid1->fetchAll();
 </head>
 
 <body>
-        
-        <?php 
-        foreach ($productid1 as $key => $productid1){
 
-                echo '<pre>';
-                print_r($productid1);
-                echo '</pre>';
-        }
+        <?php
 
-        ?> 
+
+        AffichageTables(QueryTable($db, $sqlQuery1));
+
+
+        ?>
+
 </html>
